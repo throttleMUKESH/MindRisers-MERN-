1a6c023d8aa878adcba5b96358d1de78402b468d
throttleMUKESH <chaudharymukesh370@.com>
2024-01-17 15:41:51 +0545

    Mern cls git 01

---
+++ script.js
@@ -0,0 +1,117 @@
+// let a = "namsate";
+
+// console.log(a, "ram");
+// console.log(a, "shyam");
+// console.log(a, "hari");
+// console.log(a, "gita");
+// console.log(a, "sita");
+// fullName = "MUkesh";
+
+// courseDuration = 1;
+// single = true;
+// firstName = "hari";
+// middleName = "bdr";
+// fullName = firstName + " " +  middleName;
+// console.log(fullName)
+
+
+// num = 100;
+// num2 = 20;
+// totalNum = num + num2;
+// console.log(totalNum);
+// console.log(`name:${fullName} and total:${totalNum}`);
+//  var a = 20;
+//  var a = 39;
+//  var c = a + a;
+//  console.log(a)
+//  console.log(c);
+
+// //  let x = 30;
+// //  let x  = 20;
+
+//  const z = 50;
+//  const h = z + 2;
+//  console.log(h)
+
+//  let  fruits = ['mango',"kiwi","apple"];
+// //  console.log(fruits.length);
+//  console.log(fruits.splice(1,1))
+//  console.log(fruits)
+//  let num = "seven";
+//  let str = toString()
+//  console.log(str);
+// let newFruits = fruits[2];
+// fruits[0] = "dragon fruit"
+// console.log(newFruits)
+// const course = ["mern","devops","django"];
+//   let newCourse = course[0];
+//      newCourse = "mernStack";
+//      console.log(newCourse)
+// // fruits[0] = "mern-Stack";
+// // console.log(fruits[0])
+
+
+// let str  = "123";
+//  let num = parseInt(str);
+//  console.log(num)
+
+//  let binaryString = "1010";
+// let binaryNumber = parseInt(binaryString); 
+// let userTwo = {
+//   name: "hari",
+//   phone: 9980
+// }
+
+
+// let users = [
+//   {
+
+//   data: [
+//     {
+//       name: "hari",
+//       addres: {
+//         permenant: "udayapur",
+//         temporary: "kathmandu"
+//       },
+//      contact: {
+//       NTC: true,
+//       ncell : false
+//      }
+//     }
+    
+//   ]
+
+// },
+// {
+
+//   data: [
+//     {
+//       name: "sita",
+//       addres: {
+//         permenant: "jhapa",
+//         temporary: "kathmandu"
+//       },
+//       contact: {
+//         NTC: true,
+//         ncell : true
+//        }
+
+//     }
+//   ]
+
+// }
+
+// ]
+// users[0].data[0].addres.permenant = "sindhuli"
+// console.log(users)
+
+
+
+let todo = [
+  {
+    title: "varaiable",
+    status: "complete",
+    createAt: "2024-01-01"
+  }
+]
+
